b059d779d47f91bcace8e4ddbb6d6a5c90e3ea0f
hellen atanasio <l.atanasio@hotmail.com>
2023-08-29 10:00:25 -0300

    finalizado

--- processo-seletivo/src/ProcessoSeletivo.java
+++ processo-seletivo/src/ProcessoSeletivo.java
@@ -1,9 +1,36 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MARCOS", "MARIA", "PEDRO", "PAULA"};
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
     }    
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("Contato realizado com sucesso!");
+            }
+        }while(continuarTentando && tentativasRealizadas<3);
+        if(atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " em " + tentativasRealizadas + " tentativas.");
+        }else{
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas atengido.");
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
 
     public static void imprimirSelecionados(){
         String [] candidatos = {"FELIPE", "MARCOS", "MARIA", "PEDRO", "PAULA"};
